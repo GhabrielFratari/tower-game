46cc826bf786c3f97e0389b0e8a9e31cb4bc4a86
Sandokan <sandokandias@gmail.com>
2020-03-28 17:11:25 -0300

    setup project

---
+++ Assets/Scripts/TowerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerMove : MonoBehaviour
+{
+    public float velocidade;
+    public float contadorVelocidade;
+    Vector2 deslocamento;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        deslocamento = new Vector2 (0, Time.time * velocidade);
+        GetComponent<Renderer>().material.mainTextureOffset = deslocamento;
+        contadorVelocidade += 0.0001f;
+        if(velocidade < 0.5f){
+            velocidade += 0.0001f;
+        }
+        else if(contadorVelocidade > 0.5f && velocidade < 0.7f){
+            velocidade += 0.00001f;
+        }
+        Debug.Log (velocidade);
+    }
+}
