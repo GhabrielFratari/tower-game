803d3e01ca3bf1b62a5b364252cdebd590e10230
GhabrielFratari <ghabrielfratari@gmail.com>
2021-05-05 15:48:01 -0300

    small changes
    small changes on project organization

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // "pause" the game after player's death and load the game over panel (better solution than load a whole new scene)
+    }
+
+}
