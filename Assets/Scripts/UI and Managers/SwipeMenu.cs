abc96096bedcc13120124bae0f59dd288d15422b
GhabrielFratari <ghabrielfratari@gmail.com>
2022-09-21 20:56:21 +0100

    Bouldering Tower Test

--- Assets/Scripts/UI and Managers/SwipeMenu.cs
+++ Assets/Scripts/UI and Managers/SwipeMenu.cs
@@ -41,12 +41,16 @@ public class SwipeMenu : MonoBehaviour
         {
             if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
             {
-                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
-                for(int a = 0; a < pos.Length; a++)
+                GameObject instance = transform.GetChild(i).gameObject;
+                instance.transform.localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+                instance.gameObject.GetComponent<Button>().interactable = true;
+
+                for (int a = 0; a < pos.Length; a++)
                 {
                     if(a != i)
                     {
                         transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+                        transform.GetChild(a).gameObject.GetComponent<Button>().interactable = false;
                     }
                 }
             }
